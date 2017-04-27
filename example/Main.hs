496b9a264654c22c58e5e83a4cb8c49af692a5c9
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2017-04-27 22:02:04 +0100

    Add example program

--- example/Main.hs
+++ example/Main.hs
@@ -1,11 +1,3 @@
-# Static embedded middleware
-
-A `middleware` to serve static files from memory. Works particularly
-well with `static-embedded`.
-
-## Example usage
-
-```haskell
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -17,4 +9,3 @@ main :: IO ()
 main = scotty 1337 $ do
   middleware (static $(embedDir ".")) -- serves the source directory
   notFound (text "404: Not found!")
-```
